a4655f25be5574295d58cc126acd6a3c6d4749e0
lagoke <72529997+lagoke@users.noreply.github.com>
2024-03-18 14:22:28 -0400

    added first timer feature

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,29 @@ import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter("#main-header","#main-footer","../partials/header.html","../partials/footer.html");
+// Added the event listener to the DOMContentLoaded event to ensure that the first time feature works
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = document.getElementById('registerModal');
+  const closeButton = document.querySelector('.close-button');
+
+  const showModal = () => {
+    modal.style.display = 'flex';
+    // Set a cookie for 30 days to not show the modal again
+    document.cookie = "visited=true; max-age=" + 30*24*60*60;
+  };
+
+  const closeModal = () => {
+    modal.style.display = 'none';
+  };
+
+  closeButton.addEventListener('click', closeModal);
+
+  // Check if the user has visited before
+  if (!document.cookie.split('; ').find(row => row.startsWith('visited'))) {
+    showModal();
+  }
+});
+
 
 const productCardFunc = function (product) {
   return ` 
@@ -16,6 +39,9 @@ const productCardFunc = function (product) {
       <p class="product-card__price">${product.ListPrice}</p>
     </a>
   </li>
+
+  
+  
   `;
 };
 
